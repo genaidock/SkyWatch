c5b2f0c93c1293b0ccab323d3ed6b20657f36479
SkyWatch Developer <developer@skywatch.local>
2026-07-05 01:55:54 +0530

    Fix syntax error (missing closing div) in SettingsScreen

--- src/components/screens/SettingsScreen.js
+++ src/components/screens/SettingsScreen.js
@@ -77,6 +77,7 @@ export default function SettingsScreen({ onShowToast }) {
                   </button>
                 ))}
               </div>
+            </div>
           </div>
         </div>
 
